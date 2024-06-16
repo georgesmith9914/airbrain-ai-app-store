5c57d40b3230ac6d0fa035984cfbee957d84e0d4
George Smith <georgesmith9914@gmail.com>
2024-06-16 14:44:27 +0200

    Updated

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 NODE_OPTIONS=--openssl-legacy-provider npm run build  
+export REACT_APP_ENVIRONMENT=PROD
 cd build  
 #mkdir /var/www/airbrain-website/public/appstore  
 cp -a . /var/www/airbrain-website/public/appstore
